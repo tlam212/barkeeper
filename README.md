4471d0d508027f1a16809706fdd554a2a6874624
Toni Lamont <tlam212@gmail.com>
2019-08-23 14:28:54 -0400

    cleaned up js code and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # barkeeper
+
+
+Barkeep is an app that allow bartenders to share their drink recipes with the world, as well see feedback(reviews) from others.
+
+Please be sure to run `bundle install` to ensure that all gems needed to run this app are installed.
--- barkeep_frontend/drink.js
+++ barkeep_frontend/drink.js
@@ -60,64 +60,38 @@ class Drink{
      }
 
 
-    renderToDom(){
-      let container = document.querySelector('.container')
-      let mainContainerDiv = document.createElement('div')
-      mainContainerDiv.setAttribute('class', 'mainContainer')
-      let theCardDiv = document.createElement('div')
-      theCardDiv.id = `drink-card-${this.id}`
-      theCardDiv.setAttribute('class', 'theCard')
-      let frontDiv = document.createElement('div')
-      let backDiv = document.createElement('div')
-      backDiv.setAttribute('class', 'back')
-      let reviewDiv = document.createElement('div')
-      reviewDiv.setAttribute('class', 'review')
+      renderToDom(){
+        let container = document.querySelector('.container')
+        let mainContainerDiv = document.createElement('div')
+        mainContainerDiv.setAttribute('class', 'mainContainer')
+        let theCardDiv = document.createElement('div')
+        theCardDiv.id = `drink-card-${this.id}`
+        theCardDiv.setAttribute('class', 'theCard')
+        let frontDiv = document.createElement('div')
+        let backDiv = document.createElement('div')
+        backDiv.setAttribute('class', 'back')
+        let reviewDiv = document.createElement('div')
+        reviewDiv.setAttribute('class', 'review')
 
-      frontDiv.innerHTML +=`
-      <img class="front" src='${this.image}'/>`
+        frontDiv.innerHTML +=`
+        <img class="front" src='${this.image}'/>`
 
-      backDiv.innerHTML = `
-      <h2 class='dn'>${this.name}</h2>
-      <p>${this.recipe}</p><br><br>
-      <button>See review</button>
-      `
-      backDiv.append(reviewDiv)
-      theCardDiv.append(frontDiv)
-      theCardDiv.append(backDiv)
-      mainContainerDiv.append(theCardDiv)
-      container.append(mainContainerDiv)
+        backDiv.innerHTML = `
+        <h2 class='dn'>${this.name}</h2>
+        <p>${this.recipe}</p><br><br>
+        <button>See review</button>
+        `
+        backDiv.append(reviewDiv)
+        theCardDiv.append(frontDiv)
+        theCardDiv.append(backDiv)
+        mainContainerDiv.append(theCardDiv)
+        container.append(mainContainerDiv)
 
-      backDiv.addEventListener('click', () => {
-        this.fetchDrinkReviews(event)
-      })
+        backDiv.addEventListener('click', () => {
+          this.fetchDrinkReviews(event)
+        })
 
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      }
 
 
 
